dabcc03bff17817ce7f5608457ce83ab24477593
mahallem <mahallem@PBRKZWB.corporate.act.org>
2017-02-06 00:58:41 -0600

    First Commit

---
+++ CoreJavaConcepts/src/mrm/learn/ArrayTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mrm.learn;
+
+/**
+ * @author mahallem
+ *
+ */
+public class ArrayTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+	}
+
+}
